e6f460cf59df73f8639ef8a7630b832b64f16c17
LucaAntonio <lucapistilli17@gmail.com>
2026-05-26 09:47:11 +0200

    minigioco puzzle teatro margherita

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout() {
           <Stack.Screen name="login" options={{ headerShown: false }} />
           <Stack.Screen name="register" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="minigiochi/puzzle-margherita" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Bacheca dei Trofei' }} />
         </Stack>
         <LevelUpModal />
